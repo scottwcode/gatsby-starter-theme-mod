6f5b4db5611592bf6594532c5c205c23aa4c50bb
scottwcode <scottmail77@gmail.com>
2020-09-20 22:49:20 -0400

    changes to bio-content to use shadowing from gatsby-theme-blog component

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Shadow me to add your own bio content
+ */
+
+export default () => (
+  <Fragment>
+    Words by <Styled.a href="http://example.com/">Scott W</Styled.a>.
+    <br />
+    Change is the default.
+  </Fragment>
+)
